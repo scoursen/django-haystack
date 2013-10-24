776f894b0686451d112e82e3ff6382244b4ff6b0
Steve Coursen <smcoursen@gmail.com>
2013-10-24 11:01:05 -0400

    more changes to work with older django

---
+++ haystack/encoding.py
@@ -0,0 +1,14 @@
+import six
+
+def python_2_unicode_compatible(klass):
+    """
+    A decorator that defines __unicode__ and __str__ methods under Python 2.
+    Under Python 3 it does nothing.
+
+    To support Python 2 and 3 with a single code base, define a __str__ methods    returning text and apply this decorator to the class.
+    """
+    if not six.PY3:
+        klass.__unicode__ = klass.__str__
+        klass.__str__ = lambda self: self.__unicode__().encode('utf-8')
+    return klass
+
